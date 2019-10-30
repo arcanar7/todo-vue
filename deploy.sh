d2e3906b299f395329a487a74d3586304dd42dd9
arcanar7 <arcanar7@gmail.com>
2019-10-30 15:38:17 +0400

    fix deploy gh-pages

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ set -e
 # сборка
 npm run build
 
-
+# git rm -r --cached dist
 git init
 git add dist
 git commit -m 'deploy'
+git push origin --delete gh-pages
 git subtree push --prefix dist origin gh-pages
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+//   .BundleAnalyzerPlugin
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/todo-vue/' : '/',
@@ -10,7 +10,7 @@ module.exports = {
       },
     },
   },
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()],
-  },
+  // configureWebpack: {
+  //   plugins: [new BundleAnalyzerPlugin()],
+  // },
 }
