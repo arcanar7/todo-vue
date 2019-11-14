8fd74fbbf0ba137a832864bd93dd8498946b0dcf
arcanar7 <arcanar7@gmail.com>
2019-11-14 14:33:11 +0400

    deploy

---
+++ precache-manifest.6af2454f0f07eeb8ed4b0d83c1531b7c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "81171d2e1be67dcb3a49",
+    "url": "/css/AppSpinner.component.2102543c.css"
+  },
+  {
+    "revision": "81171d2e1be67dcb3a49",
+    "url": "/js/AppSpinner.component.d4e1a79e.js"
+  },
+  {
+    "revision": "347026f23636ed9f6b1e",
+    "url": "/css/AuthPage.route.1229174a.css"
+  },
+  {
+    "revision": "347026f23636ed9f6b1e",
+    "url": "/js/AuthPage.route.d5106068.js"
+  },
+  {
+    "revision": "3d0d12172dfcf9056f29",
+    "url": "/css/HomePage.route.4fd7d58e.css"
+  },
+  {
+    "revision": "3d0d12172dfcf9056f29",
+    "url": "/js/HomePage.route.a22f16d2.js"
+  },
+  {
+    "revision": "59d0be82ae28bf218997",
+    "url": "/js/LoginPage.route.9d1dada6.js"
+  },
+  {
+    "revision": "14fc4871102ca73168e3",
+    "url": "/css/NotFoundPage.route.ed16e3ad.css"
+  },
+  {
+    "revision": "14fc4871102ca73168e3",
+    "url": "/js/NotFoundPage.route.386f370d.js"
+  },
+  {
+    "revision": "12b34f9bf0439376d08b",
+    "url": "/js/RegistrationPage.route.ec5f30f7.js"
+  },
+  {
+    "revision": "7d1e8b07b7fd98db7ce9",
+    "url": "/js/ResetPasswordPage.route.a4cceb00.js"
+  },
+  {
+    "revision": "0d436ae70bdd672c86a8",
+    "url": "/css/TodoListItemsEdit.component.f3e32368.css"
+  },
+  {
+    "revision": "0d436ae70bdd672c86a8",
+    "url": "/js/TodoListItemsEdit.component.099a0674.js"
+  },
+  {
+    "revision": "ffd4c067f165d5cb6ee0",
+    "url": "/css/app.b2c93cf6.css"
+  },
+  {
+    "revision": "ffd4c067f165d5cb6ee0",
+    "url": "/js/app.3c72ffcd.js"
+  },
+  {
+    "revision": "9478626f325bc8d040e1",
+    "url": "/css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "9478626f325bc8d040e1",
+    "url": "/js/chunk-vendors.a8c22ecf.js"
+  },
+  {
+    "revision": "b79d89a118435d06cbc3dc54312f64cf",
+    "url": "/img/arrow.b79d89a1.svg"
+  },
+  {
+    "revision": "33c24b9dacb444bbe49b45bc06d737b3",
+    "url": "/img/exit.33c24b9d.svg"
+  },
+  {
+    "revision": "3031837c6eaf94c284dd463366942d35",
+    "url": "/img/cancel.3031837c.svg"
+  },
+  {
+    "revision": "bc1816dffc2edcb4c3c899e586b97e04",
+    "url": "/img/warning.bc1816df.svg"
+  },
+  {
+    "revision": "1e0dde31125fa17575570cf6f13eadca",
+    "url": "/img/edit.1e0dde31.svg"
+  },
+  {
+    "revision": "7e63614614898521f8ea5561ae075be4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
