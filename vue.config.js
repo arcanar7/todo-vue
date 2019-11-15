f1b1c7cc55723cadcf140b195acd30c745b64a23
arcanar7 <arcanar7@gmail.com>
2019-11-15 16:11:50 +0400

    fonts

--- vue.config.js
+++ vue.config.js
@@ -22,4 +22,19 @@ module.exports = {
       },
     },
   },
+  chainWebpack: config => {
+    config.module
+      .rule('fonts')
+      .test(/\.(ttf|otf|eot|woff|woff2)$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .tap(options => {
+        options = {
+          // limit: 10000,
+          name: '/assets/fonts/[name].[ext]',
+        }
+        return options
+      })
+      .end()
+  },
 }
